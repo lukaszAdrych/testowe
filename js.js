e1c1b7374b4fd07e5f0cc5d053aac74b8cf245dd
aichosie <aichosie@gmail.com>
2015-05-09 22:57:40 +0200

    dodanie pliku js

--- js.js
+++ js.js
@@ -2,6 +2,21 @@
 
 
 
+function noweOkno() {
+    var noweOkno = window.open('', 'okno', 'toolbar=no,location=no,width=200,height=200');
+    with (noweOkno) {
+        document.writeln('<html>');
+        document.writeln('<head>');
+        document.writeln('<title>Tytuł okienka</title>');
+        document.writeln('<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />');
+        document.writeln('</head>');
+        document.writeln('<body>');
+        document.writeln('<p>To jest okno utworzone dynamicznie <strong>On-The-Fly</strong></p>');
+        document.writeln('</body>');
+        document.writeln('</html>');
+        document.close() //kończymy zapis do dokumentu
+    }
+}
 
 document.getElementById('guzikOkna').onclick = function() {
     noweOkno('strona.html');
